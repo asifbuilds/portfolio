ce387b100adc594cf26cc50005a670837c5df213
MD ASIF <asifmohammad7345@gmail.com>
2026-03-09 11:28:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Initialize Typed.js
+var typed = new Typed("#element", {
+  strings: ["Web Developer", "Graphic Designer", "Web Designer", "Video Editor"],
+  typeSpeed: 50,
+  backSpeed: 30,
+  loop: true
+});
